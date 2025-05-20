ff0f1a1b7d215de306e12a902878939c5e523d31
DanielleDelaney <sba24413@student.cct.ie>
2025-05-20 17:12:17 +0100

    d

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ ratings_file = st.file_uploader("Upload ratings.csv", type="csv")
 tags_file = st.file_uploader("Upload tags.csv", type="csv")
 df1 = pd.read_csv(movies_file, encoding ='latin1')
 df2 = pd.read_csv(ratings_file)
-df3 = pd.read_csv(tags_fil, encoding ='latin1')
+df3 = pd.read_csv(tags_file, encoding ='latin1')
 
 
     
