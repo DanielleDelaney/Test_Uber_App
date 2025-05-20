9bd2c7635ba3d301caaca12fbceffb271456dd6f
DanielleDelaney <sba24413@student.cct.ie>
2025-05-20 17:06:54 +0100

    g

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,12 @@ import plotly.express as px
 st.title("🎬 Movie Ratings Dashboard")
 
 # File upload
-movies_file = st.sidebar.file_uploader("Upload movies.csv", type="csv")
-ratings_file = st.sidebar.file_uploader("Upload ratings.csv", type="csv")
-tags_file = st.sidebar.file_uploader("Upload tags.csv", type="csv")
+movies_file = st.file_uploader("Upload movies.csv", type="csv")
+ratings_file = st.file_uploader("Upload ratings.csv", type="csv")
+tags_file = st.file_uploader("Upload tags.csv", type="csv")
+df1 = pd.read_csv(movies_file)
+df2 = pd.read_csv(ratings_file)
+df3 = pd.read_csv(tags_file)
 
 
     
